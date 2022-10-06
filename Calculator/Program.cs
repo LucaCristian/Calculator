b892f94842d76c2f5318cce6b8b524ed3cb55972
Luca Cristian <thelucutz@gmail.com>
2022-10-06 13:25:49 +0300

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = ("Calculator");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("CALCULATOR");
+
+            double nr1 = 0;
+            double nr2 = 0;
+            double result = 0;
+
+            Console.WriteLine("< NR 1 >");
+            nr1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("< NR 2 >");
+            nr2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\n< ALEGE O OPERATIE >");
+            Console.WriteLine("Adunare (+)\tScadere (-)\nInmultire (x)\tImpartire (/)");
+
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    result = nr1 + nr2;
+                    Console.WriteLine("< REZULTAT ADUNARE >\n" + result);
+                    break;
+                case "-":
+                    result = nr1 - nr2;
+                    Console.WriteLine("< REZULTAT SCADERE >\n" + result);
+                    break;
+                case "x":
+                    result = nr1 * nr2;
+                    Console.WriteLine("< REZULTAT INMULTIRE >\n" + result);
+                    break;
+                case "/":
+                    result = nr1 / nr2;
+                    Console.WriteLine("< REZULTAT IMPARTIRE >\n" + result);
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
